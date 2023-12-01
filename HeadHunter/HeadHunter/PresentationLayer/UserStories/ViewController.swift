4cc2ff803015ff4eaec8f368cdb0539fcffee293
RomanKozlovskiy <roma.kozlovskiy.96@mail.ru>
2023-12-01 19:54:45 +0300

    basic project settings

---
+++ HeadHunter/HeadHunter/PresentationLayer/UserStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeadHunter
+//
+//  Created by Роман Козловский on 01.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
